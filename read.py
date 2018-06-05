d4b883a2cc147a29b04e847e600b0b504106a9dc
Rahul Chauhan <rahulchauhan29oct@gmail.com>
2018-06-05 22:58:15 +0530

    Update read.py

--- read.py
+++ read.py
@@ -9,5 +9,5 @@ frame=cv2.VideoCapture(0)
 #defining range for required colour(these valus can be edited for the required colour)
 pts=deque(maxlen=50)
 
-Lower_green=np.array([0,100,100])
-Upper_green=np.array([10,255,255])
+Lower_green=np.array([50,100,100])
+Upper_green=np.array([70,255,255])
