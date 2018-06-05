43004161b08a7bb38e8c04fec0df3c3f04c546d0
rajkamal11 <39959030+rajkamal11@users.noreply.github.com>
2018-06-05 22:13:26 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,1 +1,13 @@
+#importing required modules
+import cv2
+from collections import deque
+import numpy as np
 
+#capturing frames from camera
+frame=cv2.VideoCapture(0)
+
+#defining range for required colour(these valus can be edited for the required colour)
+pts=deque(maxlen=50)
+
+Lower_green=np.array([0,100,100])
+Upper_green=np.array([10,255,255])
