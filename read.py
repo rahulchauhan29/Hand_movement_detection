f5c0245178483bfe9e278ed2b4c6b14baf9fd60e
Rajkamal <39959030+rajkamal11@users.noreply.github.com>
2018-06-06 15:04:20 +0530

    Update read.py

--- read.py
+++ read.py
@@ -14,3 +14,18 @@ Lower_red=np.array([0,100,100])
 Upper_red=np.array([10,255,255])
 Lower_red1=np.array([0,100,100])
 Upper_red1=np.array([10,255,255])
+
+while True:
+    ret, img=frame.read()
+    #remaining code goes here
+    
+    cv2.imshow("Frame",img)
+
+    k=cv2.waitKey(30) & 0xFF
+    if k==27: #Press"Esc" key to exit
+        break
+        
+#release videocapture object
+frame.release()
+#destroy all windows
+cv2.destroyAllWindows()        
