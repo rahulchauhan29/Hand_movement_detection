2e9a4f134b30846406b806c81d05456610f5762a
Rajkamal <39959030+rajkamal11@users.noreply.github.com>
2018-06-06 22:50:36 +0530

    Update read.py

--- read.py
+++ read.py
@@ -12,8 +12,8 @@ pts=deque(maxlen=50)
 #HSV for RED color have two ranges
 Lower_red=np.array([0,100,100])
 Upper_red=np.array([10,255,255])
-Lower_red1=np.array([0,100,100])
-Upper_red1=np.array([10,255,255])
+Lower_red1=np.array([160,100,100])
+Upper_red1=np.array([179,255,255])
 
 while True:
     ret, img=frame.read()
