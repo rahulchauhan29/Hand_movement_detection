5d90b1eadd42ce2f754a6fc24cd8ffbadb758aee
Rajkamal <39959030+rajkamal11@users.noreply.github.com>
2018-06-06 23:08:55 +0530

    Update read.py

--- read.py
+++ read.py
@@ -17,8 +17,22 @@ Upper_red1=np.array([179,255,255])
 
 while True:
     ret, img=frame.read()
-    #remaining code goes here
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    kernel=np.ones((5,5),np.uint8)
+    mask1=cv2.inRange(hsv,Lower_green,Upper_green)
+    mask2=cv2.inRange(hsv,Lower_green1,Upper_green1)
+    mask=mask1+mask2
+    mask=cv2.erode(mask,kernel,iterations=2)
+    mask=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
+    mask=cv2.dilate(mask,kernel,iterations=1)
+    res=cv2.bitwise_and(img,img,mask=mask)
+    cnts,heir=cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    center=None
     
+    #---remaining code goes here---
+    
+    cv2.imshow("mask",mask)
+    cv2.imshow("res",res)
     cv2.imshow("Frame",img)
 
     k=cv2.waitKey(30) & 0xFF
